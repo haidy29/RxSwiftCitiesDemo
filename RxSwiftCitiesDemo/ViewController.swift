3a097db5f7234517d5f8867a2df3e9547b3d376a
Haidy Saeed <haidysaeed33@gmail.com>
2025-01-14 22:41:49 +0200

    Design first Screen

---
+++ RxSwiftCitiesDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RxSwiftCitiesDemo
+//
+//  Created by Haidy Saeed on 14/01/2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
